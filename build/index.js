1ada094fda39acc20d82388b9494d87da6d300a4
Luca Micieli <rams23@live.it>
2020-03-23 13:00:32 +0100

    Build with metro contains some unwanted "import" statement

--- build/index.js
+++ build/index.js
@@ -1,7 +1,1 @@
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault"); 
-Object.defineProperty(exports, "__esModule", { value: true }); 
-exports.App = App; 
-var _cavy = require("cavy"); 
-var _react = _interopRequireDefault(require("react")); 
-var _jsxFileName = "C:\\Users\\rams2\\git\\tests\\babel-jsx\\src\\index.js"; 
-function App() { return (0, _cavy.cavyCreateElement)("div", { key: "5", __self: this, __source: { fileName: _jsxFileName, lineNumber: 5, columnNumber: 12 } }, (0, _cavy.cavyCreateElement)("div", { key: "3", __self: this, __source: { fileName: _jsxFileName, lineNumber: 5, columnNumber: 25 } }), (0, _cavy.cavyCreateElement)("div", { __self: this, __source: { fileName: _jsxFileName, lineNumber: 5, columnNumber: 44 } }, "Test")); }
+var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:true});exports.App=App;var _cavy=require("cavy");var _react=_interopRequireDefault(require("react"));var _jsxFileName="C:\\Users\\rams2\\git\\tests\\babel-jsx\\src\\index.js";function App(){return(0,_cavy.cavyCreateElement)("div",{key:"5",__self:this,__source:{fileName:_jsxFileName,lineNumber:5,columnNumber:12}},(0,_cavy.cavyCreateElement)("div",{key:"3",__self:this,__source:{fileName:_jsxFileName,lineNumber:5,columnNumber:25}}),(0,_cavy.cavyCreateElement)("div",{__self:this,__source:{fileName:_jsxFileName,lineNumber:5,columnNumber:44}},"Test"));}
